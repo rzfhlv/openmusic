b67205f86a45ead70330c2e863ccb4e764b0938e
rzfhlv <rezafahlevi.ilkom49@gmail.com>
2022-02-27 17:58:20 +0700

    add album api to server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const album = require('./api/album');
+const AlbumService = require('./services/postgres/AlbumService');
+const AlbumValidator = require('./validator/album');
 
 const init = async () => {
+    const albumService = new AlbumService();
+
     const server = Hapi.server({
        port: process.env.PORT,
        host: process.env.HOST,
@@ -20,7 +25,14 @@ const init = async () => {
             return 'Hello World';
         }
     });
-    // await server.register({});
+    
+    await server.register({
+        plugin: album,
+        options: {
+            service: albumService,
+            validator: AlbumValidator,
+        },
+    });
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
