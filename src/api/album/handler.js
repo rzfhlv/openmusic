19d21f6f3b1efa48fd73d1107b5b97b5d5faf7db
rzfhlv <rezafahlevi.ilkom49@gmail.com>
2022-02-27 19:20:09 +0700

    add edit and delete handler

--- src/api/album/handler.js
+++ src/api/album/handler.js
@@ -6,6 +6,9 @@ class AlbumHandler {
         this._validator = validator;
 
         this.postAlbumHandler = this.postAlbumHandler.bind(this);
+        this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
+        this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
+        this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
     }
 
     async postAlbumHandler(request, h) {
@@ -40,7 +43,100 @@ class AlbumHandler {
                 message: 'Something went wrong',
             });
             response.code(500);
-            console.log(error);
+            console.error(error);
+            return response;
+        }
+    }
+
+    async getAlbumByIdHandler(request, h) {
+        try {
+            const { id } = request.params;
+            const album = await this._service.getAlbumById(id);
+            return {
+                status: 'success',
+                data: {
+                    album
+                }
+            }
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server Error
+            const response = h.response({
+                status: 'error',
+                message: 'Something went wrong',
+            });
+            response.code(500);
+            console.error(error);
+            return response;
+        }
+    }
+
+    async putAlbumByIdHandler(request, h) {
+        try {
+            this._validator.validateAlbumPayload(request.payload);
+            const { id } = request.params;
+
+            await this._service.editAlbumById(id, request.payload);
+
+            return {
+                status: 'success',
+                message: 'Album success to updated',
+            };
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server Error
+            const response = h.response({
+                status: 'error',
+                message: 'Something went wrong',
+            });
+            response.code(500);
+            console.error(error);
+            return response;
+        }
+    }
+
+    async deleteAlbumByIdHandler(request, h) {
+        try {
+            const { id } = request.params;
+            await this._service.deleteAlbumById(id);
+
+            return {
+                status: 'success',
+                message: 'Album success to deleted',
+            };
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server Error
+            const response = h.response({
+                status: 'error',
+                message: 'Something went wrong',
+            });
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
