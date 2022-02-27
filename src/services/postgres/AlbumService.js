4777173e828ef7ea63bb14fcc717a1ea146766bd
rzfhlv <rezafahlevi.ilkom49@gmail.com>
2022-02-27 19:20:00 +0700

    add edit and delete albums

--- src/services/postgres/AlbumService.js
+++ src/services/postgres/AlbumService.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const { mapDBToModel } = require('../../utils/album');
 
 class AlbumService {
     constructor() {
@@ -23,6 +24,47 @@ class AlbumService {
         }
 
         return result.rows[0].id;       
+    }
+
+    async getAlbumById(id) {
+        const query = {
+            text: 'SELECT * FROM albums WHERE id = $1',
+            values: [id],
+        };
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Album not found');
+        }
+
+        return result.rows.map(mapDBToModel)[0];
+    }
+
+    async editAlbumById(id, { name, year }) {
+        const updatedAt = new Date().toISOString();
+        const query = {
+            text: 'UPDATE albums SET name = $1, year = $2, updated_at = $3 WHERE id = $4 RETURNING id',
+            values: [name, year, updatedAt, id],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Album failed to update. Id not found');
+        }
+    }
+
+    async deleteAlbumById(id) {
+        const query = {
+            text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
+            values: [id],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Album failed to delete. Id not found');
+        }
     }
 }
 
